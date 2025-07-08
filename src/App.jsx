94c33aaea25773d80f30ac6d7417cf41f65e757d
Jai Vardhan <pdmnvardhan510@gmail.com>
2025-07-07 20:41:29 -0500

    fixing render error

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+import { useState } from "react";
+import ShowDetails from "./shows/ShowDetails";
+import ShowSelection from "./shows/ShowSelection";
+import { tvShows } from "./shows/data";
+
 export default function App() {
+  const [shows, setSelectedShow] = useState(null);
   return (
     <>
       <header>
         <p>React TV</p>
+        <ShowSelection shows={tvShows} setSelectedShow={setSelectedShow} />
       </header>
-      <main></main>
+      <main>
+        <ShowDetails show={setSelectedShow}/>
+      </main>
     </>
   );
 }
--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
 import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+export default function ShowSelection({ shows, setSelectedShow }) {
+  return (
+    <nav className="shows">
+      <ul>
+        {shows.map((show) => (
+          <li
+            className="show"
+            key={show.name}
+            onClick={() => setSelectedShow(show)}
+          >
+            {show.name};
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
